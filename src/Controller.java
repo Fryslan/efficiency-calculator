3c72d676c7136f57236321474da79a1a1a695f8f
Fryslan <pietjetse@outlook.com>
2016-02-29 01:06:24 +0100

    [Upload] First initial upload.

---
+++ src/Controller.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Piet Jetse Heeringa on 28-2-2016.
+ */
+public class Controller {
+}
---
+++ src/Core.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Piet Jetse Heeringa on 28-2-2016.
+ */
+public class Core extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
